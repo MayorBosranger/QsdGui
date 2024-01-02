71416e33a49b3adb46b98adaac37719a09429819
hersleyhs <70904973+hersleyhs@users.noreply.github.com>
2024-01-02 11:40:21 +0100

    eerste poging
    testen zal gebeuren wanneer "opmaak" is gemerged

--- src/Verzoek.java
+++ src/Verzoek.java
@@ -1,13 +1,61 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Verzoek implements GuiPanel{
-    JPanel verzoekPanel;
-    public Verzoek(){
-        verzoekPanel = new JPanel();
+    public JPanel verzoekPanel;
+    private JLabel TitelLabel;
+    private JTextField queryField;
+    private JList history;
+    private JButton SendButton, ResultaatButton, OorsprongButton;
+    private JPanel ResponsePanel, HistoryPanel, DisplayPanel, DisplayInnerPanel;
+    private CardLayout cardLayout;
+    private JTable Resultaat, Oorsprong;
+    private Object ReturnedResultaat;
+    private List<String> queryHistory;
 
-        JLabel label = new JLabel("Kadapter");
-        verzoekPanel.add(label);
+    public Verzoek(){
+        SendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String query = queryField.getText();
+                queryHistory.add(query);
+                ReturnedResultaat = DoeApiRequest(query);
+            }
+        });
+        ResultaatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwitchResponsePanelTo("Resultaat");
+            }
+        });
+        OorsprongButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwitchResponsePanelTo("Oorsprong");
+            }
+        });
+        Resultaat = new JTable();
+        Oorsprong = new JTable();
+        cardLayout = new CardLayout();
+        DisplayInnerPanel = new JPanel(cardLayout);
+        DisplayPanel.add(DisplayInnerPanel);
+
+        DisplayInnerPanel.add(Resultaat, "Resultaat");
+        DisplayInnerPanel.add(Oorsprong, "Oorsprong");
+        cardLayout.show(DisplayInnerPanel, "Resultaat");
+        UpdateHistory();
+    }
+
+    public void UpdateHistory(){
+        DefaultListModel<String> listModel = new DefaultListModel<String>();
+        for (String s : queryHistory){
+            listModel.addElement(s);
+        }
+        if(listModel.isEmpty()) listModel.addElement("geschiedenis is leeg");
+        history.setModel(listModel);
     }
 
     @Override
@@ -19,5 +67,21 @@ public class Verzoek implements GuiPanel{
     @Override
     public GuiPanel getSoort() {
         return this;
+    }
+
+    public Object DoeApiRequest(String input){
+        String s = input;
+        return s;
+    }
+
+    public void SwitchResponsePanelTo(String panelNaam){
+        switch (panelNaam) {
+            case "Resultaat":
+                cardLayout.show(DisplayInnerPanel, "Resultaat");
+                break;
+            case "Oorsprong":
+                cardLayout.show(DisplayInnerPanel, "Oorsprong");
+                break;
+        }
     }
 }
