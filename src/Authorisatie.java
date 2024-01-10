8f2c1c8d262d0cb0f128b014ce088dedb8303b3c
MayorBosranger <lucmbos2@gmail.com>
2024-01-10 21:49:34 +0100

    hot garbage

--- src/Authorisatie.java
+++ src/Authorisatie.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.prefs.Preferences;
+import gateway.src.main.java.nl.geostandaarden.imx.orchestrate.gateway.*;
 
 public class Authorisatie{
     Preferences prefs = Preferences.userNodeForPackage(Authorisatie.class);
@@ -18,6 +19,21 @@ public class Authorisatie{
             @Override
             public void actionPerformed(ActionEvent e) {
                 GetKey();
+//                try {
+//                    Process proc = Runtime.getRuntime().exec("java -jar \\.Orchestrate\\imx-orchestrate-quadaster.jar");
+//                    proc.waitFor();
+//                } catch (InterruptedException ex) {
+//                    throw new RuntimeException(ex);
+//                } catch (IOException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//                ProcessBuilder pb = new ProcessBuilder("imx-orchestrate-quadaster.jar", "-jar", "imx-orchestrate-quadaster.jar");
+//                pb.directory(new File("C:\\Users\\lucmb\\Desktop\\school\\jaar 3\\GuiQsd\\QsdGui\\Orchestrate\\"));
+//                try {
+//                    Process p = pb.start();
+//                } catch (IOException ex) {
+//                    throw new RuntimeException(ex);
+//                }
             }
         });
     }
