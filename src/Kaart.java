48a29f771e8431be2943770c3f67373edf6a57d8
emir <eakkaya@live.nl>
2024-01-10 14:38:58 +0100

    positioning

--- src/Kaart.java
+++ src/Kaart.java
@@ -13,11 +13,15 @@ import javax.swing.event.MouseInputListener;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.Arrays;
 import java.util.List;
 
 public class Kaart extends JPanel {
     private JXMapViewer mapViewer;
+    private final int maximumZoomLevel = 17; // max zoomniveau
+
 
     public Kaart(){
         mapViewer = new JXMapViewer();
@@ -31,8 +35,8 @@ public class Kaart extends JPanel {
         // Set the focus
         GeoPosition frankfurt = new GeoPosition(52.5, 5.0);
 
-        mapViewer.setZoom(10);
-        mapViewer.setAddressLocation(frankfurt);
+        mapViewer.setZoom(7);
+        mapViewer.setPanEnabled(true);
 
         MouseInputListener mia = new PanMouseInputListener(mapViewer);
         mapViewer.addMouseListener(mia);
@@ -42,16 +46,16 @@ public class Kaart extends JPanel {
         mapViewer.addKeyListener(new PanKeyListener(mapViewer));
 
         List<GeoPosition> areaCoordinates = Arrays.asList(
-                new GeoPosition(54, 5.0),
-                new GeoPosition(53.6, 5.4),
-                new GeoPosition(52.1, 4.4),
-                new GeoPosition(52.3, 5.0),
-                new GeoPosition(55.2,6.5),
-                new GeoPosition(55.3,5.6)
+                new GeoPosition(52.338276,5.174459),
+                new GeoPosition(52.378324,5.173828),
+                new GeoPosition(52.394249,5.247269),
+                new GeoPosition(52.371617,5.287764)
         );
 
         OppervlaktePainter areaPainter = new OppervlaktePainter(areaCoordinates);
         mapViewer.setOverlayPainter(areaPainter);
+        mapViewer.setAddressLocation(areaPainter.berekenMiddelpunt());
+
 
         this.setLayout(new BorderLayout());
         this.add(mapViewer, BorderLayout.CENTER);
@@ -60,6 +64,16 @@ public class Kaart extends JPanel {
         this.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent componentEvent) {
                 mapViewer.setSize(Kaart.this.getSize());
+            }
+        });
+
+        mapViewer.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (e.getPreciseWheelRotation() > 0) {
+                    if(mapViewer.getZoom() > maximumZoomLevel)
+                        mapViewer.setZoom(17);
+                }
             }
         });
     }
